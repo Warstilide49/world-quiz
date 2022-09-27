5d0cf7d19d3e02b2c19b0bda6264ec96c1403dda
Warstelide49 <evindrenji01@gmail.com>
2022-09-27 10:22:13 +0530

    map with hover,click handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
-import Home from './pages/Home'
+import Home from './pages/Home';
+import Play from './pages/Play';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/world-quiz" element={<Home />} />
+        <Route path="/world-quiz/play" element={<Play />} />
+        
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/Play.js
@@ -0,0 +1,11 @@
+import Map from '../assets/WorldMap'
+
+const Play = () => {
+	return(
+		<div className='flex' id='play'>
+			<Map width="90%" height="90%"/>
+		</div>
+	)
+}
+
+export default Play
