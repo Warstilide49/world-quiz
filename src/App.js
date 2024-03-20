7431a97565356c401d9bd9ff90befaa9fc624f2a
Warstilide49 <evindrenji01@gmail.com>
2024-03-20 08:21:15 +0530

    initial firebase config

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 import countryNames from './assets/countryNames.json';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Home from './pages/Home';
 import Play from './pages/Play';
-
-const getRandomFlags = () =>{
-  const flagsToFind = []
-  while(true) {
-    let array = Object.keys(countryNames)
-    let value = array[Math.floor(Math.random() * array.length)]
-    
-    if(flagsToFind.indexOf(value) === -1) {
-      flagsToFind.push(value) 
-    }
-    if(flagsToFind.length === 5) {
-      break
-    }
-  }  
-  return flagsToFind;
-}
-
+import { app, database } from './firebaseConfig.js'
+import { collection, getDocs } from 'firebase/firestore'
 
 function App() {
 
+  console.log(process.env);
+  const collectionRef = collection(database, 'levels');
+  getDocs(collectionRef).then((response)=>{
+    console.log(response.docs.map((item)=>{
+      return item.data();
+    }))
+  })
   const [flagsToFind, setFlagsToFind] = useState([])
-
-  useEffect( () => {
-    setFlagsToFind(getRandomFlags);
-  }, [])
 
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/world-quiz" element={<Home />} />
         <Route path="/world-quiz/play" element={<Play flagsToFind={flagsToFind}/>} />
         
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,6 @@
 // import {useState, useEffect} from "react"
 import "../styles/modal.css"
+import countryNames from '../assets/countryNames.json';
 
 const Modal = (props) =>{
 
@@ -12,8 +13,8 @@ const Modal = (props) =>{
 	return(
 		<div className='modal_bg' onClick={(e) =>{setModal(false)}}>
 			<div className='modal' onClick={(e) =>{e.stopPropagation();}} style={{left: mouseCoords[0], top: mouseCoords[1]}}>
-				{countryClicked[1]}
-				<button onClick={test}>Bruh</button>
+				{countryNames[countryClicked[1]]}
+				{/* <button onClick={test}>Bruh</button> */}
 			</div>
 		</div>
 	)
