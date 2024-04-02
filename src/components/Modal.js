38c4f0f2ec51c9cf979d731b17fcf47c09f79834
Warstilide49 <evindrenji01@gmail.com>
2024-04-02 16:52:49 +0530

    fixed modal positioning

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,23 +1,32 @@
 // import {useState, useEffect} from "react"
-import "../styles/modal.css"
-import countryNames from '../assets/countryNames.json';
+import "../styles/modal.css";
+import countryNames from "../assets/countryNames.json";
 
-const Modal = (props) =>{
+const Modal = (props) => {
+    const { countryClicked, mouseCoords, setModal } = props;
 
-	const {countryClicked, mouseCoords, setModal} = props;
-
-	const test = (e) =>{
-		console.log(e.target.parentElement);
-	}
-
-	return(
-		<div className='modal_bg' onClick={(e) =>{setModal(false)}}>
-			<div className='modal' onClick={(e) =>{e.stopPropagation();}} style={{left: mouseCoords[0], top: mouseCoords[1]}}>
-				{countryNames[countryClicked[1]]}
-				{/* <button onClick={test}>Bruh</button> */}
-			</div>
-		</div>
-	)
-}
+    return (
+        <div
+            className="modal_bg"
+            onClick={(e) => {
+                setModal(false);
+            }}
+        >
+            <div
+                className="modal"
+                onClick={(e) => {
+                    e.stopPropagation();
+                }}
+                style={{
+                    left: `${mouseCoords[0] + 5}px`,
+                    top: `${mouseCoords[1] + 5}px`,
+                }}
+            >
+                {countryNames[countryClicked[1]]}
+                {/* <button onClick={test}>Bruh</button> */}
+            </div>
+        </div>
+    );
+};
 
 export default Modal;
