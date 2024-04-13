04f1044e823e1ecd63baa09004817fac392cc291
Warstilide49 <evindrenji01@gmail.com>
2024-04-13 10:13:12 +0530

    formatting

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,39 +5,49 @@ import ReactCountryFlag from "react-country-flag";
 import { useEffect, useState } from "react";
 
 const Modal = (props) => {
-  const { countryClicked, showUserModal, flagsToFind, setFlagsToFind, mouseCoords, setModal } = props;
+  const {
+    countryClicked,
+    showUserModal,
+    flagsToFind,
+    setFlagsToFind,
+    mouseCoords,
+    setModal,
+  } = props;
   const [filteredFlags, setFilteredFlags] = useState({});
-  
-  useEffect(()=> {
-    const filteredArray = Object.keys(flagsToFind).filter(objKey => !flagsToFind[objKey].isItRight).reduce((newObj, key) => {
+
+  useEffect(() => {
+    const filteredArray = Object.keys(flagsToFind)
+      .filter((objKey) => !flagsToFind[objKey].isItRight)
+      .reduce((newObj, key) => {
         newObj[key] = flagsToFind[key];
         return newObj;
       }, {});
-    setFilteredFlags(filteredArray)
-    }, [flagsToFind])
+    setFilteredFlags(filteredArray);
+  }, [flagsToFind]);
 
   const selectCountry = (selectedCode) => {
-    if(countryClicked === selectedCode) {
+    if (countryClicked === selectedCode) {
       flagsToFind[countryClicked].hasBeenSelected = true;
       flagsToFind[countryClicked].isItRight = true;
-    }
-    else {
-      if(flagsToFind[selectedCode]) {
+    } else {
+      if (flagsToFind[selectedCode]) {
         flagsToFind[selectedCode].hasBeenSelected = true;
       }
     }
-    setFlagsToFind(JSON.parse(JSON.stringify(flagsToFind)))
+    setFlagsToFind(JSON.parse(JSON.stringify(flagsToFind)));
     checkIfGameShouldEnd();
     setModal(false);
-  }
+  };
 
   const checkIfGameShouldEnd = () => {
-    const isEveryFlagGuessed = Object.keys(flagsToFind).every((country)=> flagsToFind[country].isItRight );
-    if(!isEveryFlagGuessed) {
+    const isEveryFlagGuessed = Object.keys(flagsToFind).every(
+      (country) => flagsToFind[country].isItRight,
+    );
+    if (!isEveryFlagGuessed) {
       return;
     }
-    console.log("Add fancy animation here")
-  }
+    console.log("Add fancy animation here");
+  };
 
   return (
     <div
@@ -58,22 +68,24 @@ const Modal = (props) => {
       >
         <p>Choose your answer</p>
         <div>
-        {!showUserModal &&
-          Object.keys(filteredFlags).map((code) => (
-            <ReactCountryFlag
-              key={code}
-              onClick={() => {selectCountry(code)}}
-              // className={flagsToFind[code].hasBeenSelected ? 'selected' : ""}
-              svg
-              countryCode={code}
-              title={countryNames[code]}
-              style={{
-                fontSize: "2em",
-                lineHeight: "2em",
-              }}
-              aria-label={countryNames[code]}
-            />
-          ))}
+          {!showUserModal &&
+            Object.keys(filteredFlags).map((code) => (
+              <ReactCountryFlag
+                key={code}
+                onClick={() => {
+                  selectCountry(code);
+                }}
+                // className={flagsToFind[code].hasBeenSelected ? 'selected' : ""}
+                svg
+                countryCode={code}
+                title={countryNames[code]}
+                style={{
+                  fontSize: "2em",
+                  lineHeight: "2em",
+                }}
+                aria-label={countryNames[code]}
+              />
+            ))}
         </div>
         {/* {countryNames[countryClicked]} */}
         {/* <button onClick={test}>Bruh</button> */}
--- src/pages/Play.js
+++ src/pages/Play.js
@@ -4,34 +4,34 @@ import countryNames from "../assets/countryNames.json";
 import { useEffect, useState } from "react";
 import UserInfo from "../components/UserInfo";
 import styles from "../styles/play.module.css";
-import tickMark from "../assets/icons/check-mark 1.svg"
-import wrongMark from "../assets/icons/no 1.svg"
+import tickMark from "../assets/icons/check-mark 1.svg";
+import wrongMark from "../assets/icons/no 1.svg";
 
 const Play = ({ allChoices }) => {
   const [username, setUsername] = useState("");
   const [level, setLevel] = useState("Easy");
   const [showUserModal, setShowUserModal] = useState(null);
   const [flagsToFind, setFlagsToFind] = useState({});
-  
+
   useEffect(() => {
     setShowUserModal(true);
   }, []);
-  
+
   useEffect(() => {
     const LEVELS = ["Easy", "Medium", "Hard"];
-    const countries = allChoices[LEVELS.findIndex((element) => element === level)] || [];
-    Object.keys(countries).forEach((key)=> {
+    const countries =
+      allChoices[LEVELS.findIndex((element) => element === level)] || [];
+    Object.keys(countries).forEach((key) => {
       countries[key] = {
-        countryName : countries[key],
+        countryName: countries[key],
         hasBeenSelected: false,
-        isItRight: false
+        isItRight: false,
         // Maybe will add this later
-        // chances: 3 
-      }
-    })
+        // chances: 3
+      };
+    });
     setFlagsToFind(countries);
   }, [allChoices, level]);
-
 
   return (
     <div className="flex flex-column" id="play">
@@ -44,25 +44,49 @@ const Play = ({ allChoices }) => {
         />
       )}
       <div className={styles.flagHolder}>
-      { !showUserModal && Object.keys(flagsToFind).map((code) => (
+        {!showUserModal &&
+          Object.keys(flagsToFind).map((code) => (
             <div className="flag-container" key={code}>
               <ReactCountryFlag
                 svg
-                className={`emoji-flag ${flagsToFind[code].hasBeenSelected ? 'selected' : ''}`}
+                className={`emoji-flag ${flagsToFind[code].hasBeenSelected ? "selected" : ""}`}
                 countryCode={code}
                 title={countryNames[code]}
                 style={{
-                  fontSize: "2em"
+                  fontSize: "2em",
                 }}
                 aria-label={countryNames[code]}
               />
-              {flagsToFind[code].isItRight && flagsToFind[code].hasBeenSelected && <img src={tickMark} className="flag-result" alt="Correct" width="20px" height="20px"></img>}
-              {!flagsToFind[code].isItRight && flagsToFind[code].hasBeenSelected && <img src={wrongMark} className="flag-result" alt="Wrong" width="20px" height="20px"></img>}
+              {flagsToFind[code].isItRight &&
+                flagsToFind[code].hasBeenSelected && (
+                  <img
+                    src={tickMark}
+                    className="flag-result"
+                    alt="Correct"
+                    width="20px"
+                    height="20px"
+                  ></img>
+                )}
+              {!flagsToFind[code].isItRight &&
+                flagsToFind[code].hasBeenSelected && (
+                  <img
+                    src={wrongMark}
+                    className="flag-result"
+                    alt="Wrong"
+                    width="20px"
+                    height="20px"
+                  ></img>
+                )}
             </div>
           ))}
-        
       </div>
-      <Map width="85vw" height="85vh" flagsToFind={flagsToFind} setFlagsToFind={setFlagsToFind} showUserModal={showUserModal} />
+      <Map
+        width="85vw"
+        height="85vh"
+        flagsToFind={flagsToFind}
+        setFlagsToFind={setFlagsToFind}
+        showUserModal={showUserModal}
+      />
     </div>
   );
 };
